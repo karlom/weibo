56aa3c1e4826ce5e109ff0be5034a8fa5f944148
karlom <karlomgg@gmail.com>
2021-04-23 06:26:48 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('signup', 'UsersController@create')->name('signup');
 
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
