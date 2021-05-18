c4915048a7bef3505c2fa0d909e7e4e32d1b0965
karlom <karlomgg@gmail.com>
2021-05-18 08:45:59 +0000

    管理员可以删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -7,11 +7,6 @@ use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         User::factory()->count(50)->create();
@@ -19,6 +14,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
